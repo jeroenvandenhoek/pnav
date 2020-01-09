51a54d34cbcc4388703b83e2457905bf98508280
root <root@vultr.guest>
2020-01-09 08:42:51 +0000

    navigation tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::fs;
+use std::error::Error;
 mod process_arguments;
 mod manipulate_pnavrc;
 mod contextual_info;
@@ -6,5 +8,9 @@ mod create_project_directories;
 
 pub fn run(args: Vec<String>) {
     manipulate_pnavrc::manip_pnavrc();
-    navigate_folders::get_project_input_folder("002003", "/var");
+    let client_input_folder: Result<fs::DirEntry, _> = navigate_folders::get_project_input_folder("150", "/root/pnav_project_root_for_testing");
+    let project_input_folder: Result<fs::DirEntry, _> = navigate_folders::get_project_input_folder("150001", "/root/pnav_project_root_for_testing");
+
+    println!("{:?}",client_input_folder);
+    println!("{:?}",project_input_folder);
 }
