fece03ca4070084b95705851e536b5d9256bddaf
root <root@vultr.guest>
2020-01-11 14:28:57 +0000

    fixed wrong reference

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,12 @@ use std::error::Error;
 mod process_arguments;
 mod manipulate_pnavrc;
 mod contextual_info;
-mod navigate_folders;
+pub mod navigate_folders;
 mod create_project_directories;
 
+/// runs the pnav program
 pub fn run(args: Vec<String>) {
-    manipulate_pnavrc::manip_pnavrc();
+    manipulate_pnavrc::read_pnavrc();
     let client_input_folder: Result<fs::DirEntry, _> = navigate_folders::get_project_input_folder("150", "/root/pnav_project_root_for_testing");
     let project_input_folder: Result<fs::DirEntry, _> = navigate_folders::get_project_input_folder("150001", "/root/pnav_project_root_for_testing");
 
--- src/manipulate_pnavrc.rs
+++ src/manipulate_pnavrc.rs
@@ -1,8 +1,8 @@
 use std::fs;
 
+pub fn read_pnavrc() {}
+
 pub fn create_pnavrc() {}
 
 pub fn write_pnavrc() {}
-
-pub fn read_pnavrc() {}
 
