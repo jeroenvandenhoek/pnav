7265c40996c790dc0fb18ed04fc0e9201c197ff4
root <root@vultr.guest>
2020-01-12 20:13:01 +0000

    set up structure for input.rs

--- src/input.rs
+++ src/input.rs
@@ -1,14 +1,44 @@
-struct Input {
-    arguments: Vec<&str>,
-    flags_general: Vec<&str>,
-    flags_targeting_project: Vec<&str>,
-    flags_targeting_production: Vec<&str>,
-    config_project_root: &str,
-    config_production_roots: Vec<&stra>,
-    config_active_project: &str,
-    config_company_name: &str
+use std::error::Error;
+
+pub struct Input <'a> {
+    arguments: Option<Vec<&'a str>>,
+    flags_general: Option<Vec<&'a str>>,
+    flags_targeting_project: Option<Vec<&'a str>>,
+    flags_targeting_production: Option<Vec<&'a str>>,
+    config_project_root: Option<&'a str>,
+    config_production_roots: Option<Vec<&'a str>>,
+    config_active_project: Option<&'a str>,
+    config_company_name: Option<&'a str>
 }
 
-impl Input {
-    fn parse_args(&mut str) {}
+impl<'a> Input <'a>{
+    pub fn get() -> Input<'a>{
+        let mut Input = Input{
+            arguments: None,
+            flags_general: None,
+            flags_targeting_project: None,
+            flags_targeting_production: None,
+            config_project_root: None,
+            config_production_roots: None,
+            config_active_project: None,
+            config_company_name: None
+        }; 
+
+        Input.parse_args();
+
+        Input
+    }
 }
+impl<'a> Input<'a> {
+    fn parse_args(&mut self) -> Option<Vec<&str>> {
+
+        None
+    }
+    fn parse_flags(&mut self) {}
+    fn iparse_config(&mut self) {}
+    fn ask_config() {}
+    fn write_config() {}
+
+}
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use std::error::Error;
+mod input;
 
 /// runs the pnav program
 pub fn run() {
